52a26d86b9ee06ee7990cdeba99fad5f53a1cad2
TimVanMourik <timvanmourik@gmail.com>
2017-03-14 16:11:16 +0100

    Licensing information added!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-/* Copyright (c) 2014, Tim van Mourik
+
+/* Copyright (C) Tim van Mourik, 2014-2016
+ * Copyright (C) Tim van Mourik, 2017, DCCN
  * All rights reserved
  *
  * This program has been created from scratch, but was inspired by the following packages:
@@ -16,6 +18,30 @@
  *
  * To my knowledge non of these ideas or examples are patented or need to be referenced explicitly in the way I have used them.
  */
+
+
+/*  Copyright (C) Tim van Mourik, 2014-2016
+    Copyright (C) Tim van Mourik, 2017, DCCN
+    All rights reserved
+
+ This file is part of the Porcupine pipeline tool, see
+ https://github.com/TimVanMourik/Porcupine for the documentation and
+ details.
+
+    This toolbox is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This toolbox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the fmri analysis toolbox. If not, see
+    <http://www.gnu.org/licenses/>.
+*/
 
 #include "MainWindow.hpp"
 #include <QApplication>
--- src/Settings/DataType.cpp
+++ src/Settings/DataType.cpp
@@ -1,3 +1,26 @@
+/*  Copyright (C) Tim van Mourik, 2014-2016
+    Copyright (C) Tim van Mourik, 2017, DCCN
+    All rights reserved
+
+ This file is part of the Porcupine pipeline tool, see
+ https://github.com/TimVanMourik/Porcupine for the documentation and
+ details.
+
+    This toolbox is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This toolbox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the fmri analysis toolbox. If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+
 #include "DataType.hpp"
 
 DataType::DataType(
