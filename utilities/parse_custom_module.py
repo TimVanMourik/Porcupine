36f10a21d2c05d7409961c7a9dbdc0e0ccaf5de8
Lukas Snoek <lukassnoek@gmail.com>
2017-07-04 14:34:11 +0200

    Improve parser

--- utilities/parse_custom_module.py
+++ utilities/parse_custom_module.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 import inspect
 import importlib
@@ -5,27 +6,34 @@ import os.path as op
 import json
 from nipype2json import node2json
 
-
-def custommodule2json(module_path):
+def custommodule2json(module_path, verbose=False):
 
     sys.path.append(op.dirname(module_path))
     module_to_import = op.basename(module_path).replace('.py', '')
     module = importlib.import_module(module_to_import)
     module_name = module.__name__
+    print("Parsing custom module %s ..." % module_name)
 
     jsons = []
     for node in dir(module):
+        
         node_inst = getattr(module, node)
         node_class = node_inst.__class__
+        
         if str(node_class) == "<class 'nipype.pipeline.engine.nodes.Node'>":
+                
+            if verbose:
+                print("Adding node '%s'" % node)
+            
             jsons.append(node2json(node_inst, custom_node=True,
                                    module=module_name,
                                    category='Custom'))
 
-    out_fn = op.join(op.dirname(module_path), module_name + '.JSON')
+    out_fn = op.abspath(op.join(op.dirname(module_path), module_name + '.JSON'))
+    print("Writing nodes to %s ..." % out_fn, end='')
     with open(out_fn, 'w') as outfile:
         json.dump({'nodes': jsons}, outfile, sort_keys=False, indent=2)
-
+    print(' done.')
 
 if __name__ == '__main__':
 
@@ -33,6 +41,8 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Parses custom modules with Nipype nodes.')
     parser.add_argument('-m', dest='module', type=str,
                         help="Module to parse")
-    parser.parse_args()
-
-    custommodule2json(parser.module)
+    parser.add_argument('-v', dest='verbose', action='store_true',
+                        help="Print much more output ...")
+    
+    args = parser.parse_args()
+    custommodule2json(args.module, args.verbose)
