350fee697ad6d9f613c5507f7de55969410ddb43
Lukas Snoek <lukassnoek@gmail.com>
2017-08-24 17:32:40 +0200

    Update examples part 2

--- example_workflows/resting_preproc_fsl/porcupine_generated_workflow.py
+++ example_workflows/resting_preproc_fsl/porcupine_generated_workflow.py
@@ -1,4 +1,5 @@
-#This is a NiPype generator. Warning, here be dragons.
+#This is a Nipype generator. Warning, here be dragons.
+import sys
 import nipype
 import nipype.pipeline as pe
 import nipype.interfaces.io as io
@@ -8,60 +9,64 @@ import nipype.algorithms.confounds as confounds
 WorkingDirectory = "~/Porcupipelines/ThisStudy"
 
 #Generic datagrabber module that wraps around glob in an
-NodeHash_2f40f80 = pe.Node(io.S3DataGrabber(outfields=['outfiles']), name = 'NodeName_2f40f80')
-NodeHash_2f40f80.inputs.anon = True
-NodeHash_2f40f80.inputs.bucket = 'openneuro'
-NodeHash_2f40f80.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
-NodeHash_2f40f80.inputs.local_directory = '/tmp'
-NodeHash_2f40f80.inputs.sort_filelist = True
-NodeHash_2f40f80.inputs.template = 'sub-01/func/sub-01_task-simon_run-1_bold.nii.gz'
+NodeHash_f967c10 = pe.Node(io.S3DataGrabber(outfields=['outfiles']), name = 'NodeName_f967c10')
+NodeHash_f967c10.inputs.bucket = 'openneuro'
+NodeHash_f967c10.inputs.sort_filelist = True
+NodeHash_f967c10.inputs.template = 'sub-01/func/sub-01_task-simon_run-1_bold.nii.gz'
+NodeHash_f967c10.inputs.anon = True
+NodeHash_f967c10.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
+NodeHash_f967c10.inputs.local_directory = '/tmp'
 
 #Wraps command **slicetimer**
-NodeHash_258f950 = pe.Node(interface = fsl.SliceTimer(), name = 'NodeName_258f950')
+NodeHash_1b357220 = pe.Node(interface = fsl.SliceTimer(), name = 'NodeName_1b357220')
 
 #Wraps command **mcflirt**
-NodeHash_40a99f0 = pe.Node(interface = fsl.MCFLIRT(), name = 'NodeName_40a99f0')
+NodeHash_1b961070 = pe.Node(interface = fsl.MCFLIRT(), name = 'NodeName_1b961070')
 
 #Computes the time-course SNR for a time series
-NodeHash_5e7db90 = pe.Node(interface = confounds.TSNR(), name = 'NodeName_5e7db90')
-NodeHash_5e7db90.inputs.regress_poly = 2
+NodeHash_203cd2f0 = pe.Node(interface = confounds.TSNR(), name = 'NodeName_203cd2f0')
+NodeHash_203cd2f0.inputs.regress_poly = 2
 
 #Wraps command **fslstats**
-NodeHash_2c4d280 = pe.Node(interface = fsl.ImageStats(), name = 'NodeName_2c4d280')
-NodeHash_2c4d280.inputs.op_string = '-p 98'
+NodeHash_1e7acf80 = pe.Node(interface = fsl.ImageStats(), name = 'NodeName_1e7acf80')
+NodeHash_1e7acf80.inputs.op_string = '-p 98'
 
 #Wraps command **fslmaths**
-NodeHash_38a9b60 = pe.Node(interface = fsl.Threshold(), name = 'NodeName_38a9b60')
+NodeHash_1d00edc0 = pe.Node(interface = fsl.Threshold(), name = 'NodeName_1d00edc0')
 
 #Anatomical compcor: for inputs and outputs, see CompCor.
-NodeHash_5e38960 = pe.Node(interface = confounds.ACompCor(), name = 'NodeName_5e38960')
-NodeHash_5e38960.inputs.num_components = 3
+NodeHash_1d3982c0 = pe.Node(interface = confounds.ACompCor(), name = 'NodeName_1d3982c0')
+NodeHash_1d3982c0.inputs.num_components = 3
 
 #Wraps command **fsl_regfilt**
-NodeHash_9ee9f70 = pe.Node(interface = fsl.FilterRegressor(), name = 'NodeName_9ee9f70')
-NodeHash_9ee9f70.inputs.filter_columns = [1, 2]
+NodeHash_23e60ee0 = pe.Node(interface = fsl.FilterRegressor(), name = 'NodeName_23e60ee0')
+NodeHash_23e60ee0.inputs.filter_columns = [1, 2]
 
 #Wraps command **fslmaths**
-NodeHash_addaf50 = pe.Node(interface = fsl.TemporalFilter(), name = 'NodeName_addaf50')
+NodeHash_23e6e8c0 = pe.Node(interface = fsl.TemporalFilter(), name = 'NodeName_23e6e8c0')
+NodeHash_23e6e8c0.inputs.lowpass_sigma = 3
 
 #Generic datasink module to store structured outputs
-NodeHash_38bd8e0 = pe.Node(interface = io.DataSink(), name = 'NodeName_38bd8e0')
-NodeHash_38bd8e0.inputs.base_directory = '/tmp'
+NodeHash_23e9e530 = pe.Node(interface = io.DataSink(), name = 'NodeName_23e9e530')
+NodeHash_23e9e530.inputs.base_directory = '/tmp'
 
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-analysisflow.connect(NodeHash_5e38960, 'components_file', NodeHash_9ee9f70, 'design_file')
-analysisflow.connect(NodeHash_addaf50, 'out_file', NodeHash_38bd8e0, 'filtered_file')
-analysisflow.connect(NodeHash_9ee9f70, 'out_file', NodeHash_addaf50, 'in_file')
-analysisflow.connect(NodeHash_5e7db90, 'detrended_file', NodeHash_9ee9f70, 'in_file')
-analysisflow.connect(NodeHash_38a9b60, 'out_file', NodeHash_5e38960, 'mask_files')
-analysisflow.connect(NodeHash_40a99f0, 'out_file', NodeHash_5e38960, 'realigned_file')
-analysisflow.connect(NodeHash_2c4d280, 'out_stat', NodeHash_38a9b60, 'thresh')
-analysisflow.connect(NodeHash_5e7db90, 'stddev_file', NodeHash_2c4d280, 'in_file')
-analysisflow.connect(NodeHash_5e7db90, 'stddev_file', NodeHash_38a9b60, 'in_file')
-analysisflow.connect(NodeHash_40a99f0, 'out_file', NodeHash_5e7db90, 'in_file')
-analysisflow.connect(NodeHash_258f950, 'slice_time_corrected_file', NodeHash_40a99f0, 'in_file')
-analysisflow.connect(NodeHash_2f40f80, 'outfiles', NodeHash_258f950, 'in_file')
+analysisflow.connect(NodeHash_23e6e8c0, 'out_file', NodeHash_23e9e530, 'rs_clean')
+analysisflow.connect(NodeHash_23e60ee0, 'out_file', NodeHash_23e6e8c0, 'in_file')
+analysisflow.connect(NodeHash_203cd2f0, 'detrended_file', NodeHash_23e60ee0, 'in_file')
+analysisflow.connect(NodeHash_1d3982c0, 'components_file', NodeHash_23e60ee0, 'design_file')
+analysisflow.connect(NodeHash_1b357220, 'slice_time_corrected_file', NodeHash_1d3982c0, 'realigned_file')
+analysisflow.connect(NodeHash_1d00edc0, 'out_file', NodeHash_1d3982c0, 'mask_files')
+analysisflow.connect(NodeHash_203cd2f0, 'stddev_file', NodeHash_1d00edc0, 'in_file')
+analysisflow.connect(NodeHash_1e7acf80, 'out_stat', NodeHash_1d00edc0, 'thresh')
+analysisflow.connect(NodeHash_203cd2f0, 'stddev_file', NodeHash_1e7acf80, 'in_file')
+analysisflow.connect(NodeHash_1b961070, 'out_file', NodeHash_203cd2f0, 'in_file')
+analysisflow.connect(NodeHash_1b357220, 'slice_time_corrected_file', NodeHash_1b961070, 'in_file')
+analysisflow.connect(NodeHash_f967c10, 'outfiles', NodeHash_1b357220, 'in_file')
 
 #Run the workflow
-analysisflow.run()
+plugin = 'MultiProc' #adjust your desired plugin here
+plugin_args = {'n_procs': 1} #adjust to your number of cores
+analysisflow.write_graph(graph2use='flat', format='png', simple_form=False)
+analysisflow.run(plugin=plugin, plugin_args=plugin_args)
--- example_workflows/skullstrip_afni_ants/porcupine_generated_pipeline.py
+++ example_workflows/skullstrip_afni_ants/porcupine_generated_pipeline.py
@@ -1,4 +1,5 @@
-#This is a NiPype generator. Warning, here be dragons.
+#This is a Nipype generator. Warning, here be dragons.
+import sys
 import nipype
 import nipype.pipeline as pe
 import nipype.interfaces.io as io
@@ -9,58 +10,63 @@ import nipype.interfaces.fsl as fsl
 WorkingDirectory = "~/Porcupipelines/ThisStudy"
 
 #Generic datagrabber module that wraps around glob in an
-NodeHash_2d63780 = pe.Node(io.S3DataGrabber(), name = 'NodeName_2d63780')
-NodeHash_2d63780.inputs.anon = True
-NodeHash_2d63780.inputs.bucket = 'openneuro'
-NodeHash_2d63780.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
-NodeHash_2d63780.inputs.local_directory = '/tmp'
-NodeHash_2d63780.inputs.sort_filelist = True
-NodeHash_2d63780.inputs.template = 'sub-01/anat/sub-01_T1w.nii.gz'
+NodeHash_84e35c0 = pe.Node(io.S3DataGrabber(outfields=['outfiles']), name = 'NodeName_84e35c0')
+NodeHash_84e35c0.inputs.bucket = 'openneuro'
+NodeHash_84e35c0.inputs.sort_filelist = True
+NodeHash_84e35c0.inputs.template = 'sub-01/anat/sub-01_T1w.nii.gz'
+NodeHash_84e35c0.inputs.anon = True
+NodeHash_84e35c0.inputs.bucket_path = 'ds000101/ds000101_R2.0.0/uncompressed/'
+NodeHash_84e35c0.inputs.local_directory = '/tmp'
 
 #Wraps command **N4BiasFieldCorrection**
-NodeHash_2cbfbc0 = pe.Node(interface = ants.N4BiasFieldCorrection(), name = 'NodeName_2cbfbc0')
-NodeHash_2cbfbc0.inputs.save_bias = True
-NodeHash_2cbfbc0.inputs.dimension = 3
-NodeHash_2cbfbc0.inputs.num_threads = 4
+NodeHash_fff29c0 = pe.Node(interface = ants.N4BiasFieldCorrection(), name = 'NodeName_fff29c0')
+NodeHash_fff29c0.inputs.save_bias = True
+NodeHash_fff29c0.inputs.copy_header = False
+NodeHash_fff29c0.inputs.dimension = 3
+NodeHash_fff29c0.inputs.num_threads = 4
 
 #Wraps command **3dUnifize**
-NodeHash_6ae34f0 = pe.Node(interface = afni.Unifize(), name = 'NodeName_6ae34f0')
-NodeHash_6ae34f0.inputs.outputtype = 'NIFTI_GZ'
+NodeHash_f966e70 = pe.Node(interface = afni.Unifize(), name = 'NodeName_f966e70')
+NodeHash_f966e70.inputs.outputtype = 'NIFTI_GZ'
 
 #Wraps command **3dSkullStrip**
-NodeHash_5770380 = pe.Node(interface = afni.SkullStrip(), name = 'NodeName_5770380')
-NodeHash_5770380.inputs.outputtype = 'NIFTI_GZ'
+NodeHash_132bc280 = pe.Node(interface = afni.SkullStrip(), name = 'NodeName_132bc280')
+NodeHash_132bc280.inputs.outputtype = 'NIFTI_GZ'
 
 #Wraps command **3dcalc**
-NodeHash_2c7fe50 = pe.Node(interface = afni.Calc(), name = 'NodeName_2c7fe50')
-NodeHash_2c7fe50.inputs.expr = 'a*step(b)'
-NodeHash_2c7fe50.inputs.outputtype = 'NIFTI_GZ'
-
-#Wraps command **3dUnifize**
-NodeHash_6ad7e30 = pe.Node(interface = afni.Unifize(), name = 'NodeName_6ad7e30')
-NodeHash_6ad7e30.inputs.gm = True
-NodeHash_6ad7e30.inputs.outputtype = 'NIFTI_GZ'
+NodeHash_14d89eb0 = pe.Node(interface = afni.Calc(), name = 'NodeName_14d89eb0')
+NodeHash_14d89eb0.inputs.expr = 'a*step(b)'
+NodeHash_14d89eb0.inputs.outputtype = 'NIFTI_GZ'
 
 #Wraps command **fslmaths**
-NodeHash_4f71290 = pe.Node(interface = fsl.Threshold(), name = 'NodeName_4f71290')
-NodeHash_4f71290.inputs.thresh = 1.e-3
-NodeHash_4f71290.inputs.args = '-bin'
+NodeHash_1758c760 = pe.Node(interface = fsl.Threshold(), name = 'NodeName_1758c760')
+NodeHash_1758c760.inputs.thresh = 1.e-3
+NodeHash_1758c760.inputs.args = '-bin'
+
+#Wraps command **3dUnifize**
+NodeHash_14c85ce0 = pe.Node(interface = afni.Unifize(), name = 'NodeName_14c85ce0')
+NodeHash_14c85ce0.inputs.gm = True
+NodeHash_14c85ce0.inputs.outputtype = 'NIFTI_GZ'
 
 #Generic datasink module to store structured outputs
-NodeHash_3b95bb0 = pe.Node(interface = io.DataSink(), name = 'NodeName_3b95bb0')
+NodeHash_11a54520 = pe.Node(interface = io.DataSink(), name = 'NodeName_11a54520')
+NodeHash_11a54520.inputs.base_directory = '/tmp'
 
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
-analysisflow.connect(NodeHash_2cbfbc0, 'bias_image', NodeHash_3b95bb0, 'bias_image')
-analysisflow.connect(NodeHash_6ad7e30, 'out_file', NodeHash_3b95bb0, 'out_file')
-analysisflow.connect(NodeHash_4f71290, 'out_file', NodeHash_3b95bb0, 'out_mask')
-analysisflow.connect(NodeHash_2c7fe50, 'out_file', NodeHash_4f71290, 'in_file')
-analysisflow.connect(NodeHash_5770380, 'out_file', NodeHash_2c7fe50, 'in_file_b')
-analysisflow.connect(NodeHash_2c7fe50, 'out_file', NodeHash_6ad7e30, 'in_file')
-analysisflow.connect(NodeHash_6ae34f0, 'out_file', NodeHash_2c7fe50, 'in_file_a')
-analysisflow.connect(NodeHash_6ae34f0, 'out_file', NodeHash_5770380, 'in_file')
-analysisflow.connect(NodeHash_2cbfbc0, 'output_image', NodeHash_6ae34f0, 'in_file')
-analysisflow.connect(NodeHash_2d63780, 'outfiles', NodeHash_2cbfbc0, 'input_image')
+analysisflow.connect(NodeHash_14c85ce0, 'out_file', NodeHash_11a54520, 'out_file')
+analysisflow.connect(NodeHash_1758c760, 'out_file', NodeHash_11a54520, 'out_mask')
+analysisflow.connect(NodeHash_14d89eb0, 'out_file', NodeHash_14c85ce0, 'in_file')
+analysisflow.connect(NodeHash_14d89eb0, 'out_file', NodeHash_1758c760, 'in_file')
+analysisflow.connect(NodeHash_132bc280, 'out_file', NodeHash_14d89eb0, 'in_file_b')
+analysisflow.connect(NodeHash_f966e70, 'out_file', NodeHash_14d89eb0, 'in_file_a')
+analysisflow.connect(NodeHash_fff29c0, 'bias_image', NodeHash_11a54520, 'bias_image')
+analysisflow.connect(NodeHash_f966e70, 'out_file', NodeHash_132bc280, 'in_file')
+analysisflow.connect(NodeHash_fff29c0, 'output_image', NodeHash_f966e70, 'in_file')
+analysisflow.connect(NodeHash_84e35c0, 'outfiles', NodeHash_fff29c0, 'input_image')
 
 #Run the workflow
-analysisflow.run()
+plugin = 'MultiProc' #adjust your desired plugin here
+plugin_args = {'n_procs': 1} #adjust to your number of cores
+analysisflow.write_graph(graph2use='flat', format='png', simple_form=False)
+analysisflow.run(plugin=plugin, plugin_args=plugin_args)
